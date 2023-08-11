4a0aada99a8dfdc6be02d6630d8a1a259becbc2c
Enzo Vieira <enzovieira16@hotmail.com>
2023-08-11 01:02:58 -0300

    add: History page

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,35 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {[...Array(5)].map((_, index) => (
+              <tr key={index}>
+                <td>Tarefa</td>
+                <td>20 minutos</td>
+                <td>Há dois meses</td>
+                <td>
+                  <Status statusColor="green">Concluído</Status>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
