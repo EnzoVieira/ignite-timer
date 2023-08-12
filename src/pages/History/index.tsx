94a608393a2066a90102c9d9e5a2a79e7c84dec3
Enzo Vieira <enzovieira16@hotmail.com>
2023-08-11 21:18:40 -0300

    feat(Hystory): Fetch cycles from context

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,6 @@
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { useCycles } from '../../context/CyclesContext'
 import { HistoryContainer, HistoryList, Status } from './styles'
 
@@ -20,13 +23,27 @@ export function History() {
           </thead>
 
           <tbody>
-            {[...Array(5)].map((_, index) => (
-              <tr key={index}>
-                <td>Tarefa</td>
-                <td>20 minutos</td>
-                <td>Há dois meses</td>
+            {cycles.map((cycle) => (
+              <tr key={cycle.id}>
+                <td>{cycle.task}</td>
+                <td>{cycle.minutesAmount} minutos</td>
                 <td>
-                  <Status statusColor="green">Concluído</Status>
+                  {formatDistanceToNow(cycle.startDate, {
+                    addSuffix: true,
+                    locale: ptBR,
+                  })}
+                </td>
+                <td>
+                  {cycle.finishedDate && (
+                    <Status statusColor="green">Concluído</Status>
+                  )}
+                  {cycle.interruptedDate && (
+                    <Status statusColor="red">Interrompido</Status>
+                  )}
+
+                  {!cycle.interruptedDate && !cycle.finishedDate && (
+                    <Status statusColor="yellow">Em andamento</Status>
+                  )}
                 </td>
               </tr>
             ))}
